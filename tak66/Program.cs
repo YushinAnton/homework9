f6c3374f27fc8fca0ef59be2052f446eff7e8e58
Anton_Yushin <ya.napishimne@yandex.ru>
2023-05-27 10:53:16 +0300

    66

---
+++ tak66/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 66: Задайте значения M и N.
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+
+int SumMN(int m, int n)
+{
+    int result = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        result = m + SumMN(m, n);
+        return result;
+    }
+}
+
+Console.WriteLine("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(SumMN(m - 1, n));
